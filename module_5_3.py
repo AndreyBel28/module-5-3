f7f83676d9034fa4615b9e7a93f195c1abafe31f
Андрей <omarov.a9609@gmail.com>
2025-01-16 21:52:40 +0500

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -5,44 +5,65 @@ class House:
     def __eq__(self, other):
         if isinstance(other, House):
             return self.floors == other.floors
+        elif isinstance(other, int):
+            return self.floors == other
         return False
+
+    def __add__(self, other):
+        if isinstance(other, House):
+            self.floors += other.floors
+        elif isinstance(other, int):
+            self.floors += other
+        return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
 
     def __lt__(self, other):
         if isinstance(other, House):
             return self.floors < other.floors
+        elif isinstance(other, int):
+            return self.floors < other
         return NotImplemented
 
     def __le__(self, other):
         if isinstance(other, House):
             return self.floors <= other.floors
+        elif isinstance(other, int):
+            return self.floors <= other
         return NotImplemented
 
     def __gt__(self, other):
         if isinstance(other, House):
             return self.floors > other.floors
+        elif isinstance(other, int):
+            return self.floors > other
         return NotImplemented
 
     def __ge__(self, other):
         if isinstance(other, House):
             return self.floors >= other.floors
+        elif isinstance(other, int):
+            return self.floors >= other
         return NotImplemented
 
     def __ne__(self, other):
         if isinstance(other, House):
             return self.floors != other.floors
+        elif isinstance(other, int):
+            return self.floors != other
         return NotImplemented
-
-    def __add__(self, value):
-        if isinstance(value, int):
-            self.floors += value
-            return self
-        return NotImplemented
-
-    def __radd__(self, value):
-        return self.__add__(value)
-
-    def __iadd__(self, value):
-        return self.__add__(value)
 
     def __str__(self):
         return f'House with {self.floors} floors'
+
+house1 = House(2)
+house2 = House(3)
+
+print(house1 + house2)
+print(house1 + 1)
+print(house2 == 3)
+print(house1 < 3)
